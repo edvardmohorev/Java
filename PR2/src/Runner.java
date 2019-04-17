908c1f5074d28b444557497f2e25f28a9fd00ef7
Edvard <47501648+JIuckman@users.noreply.github.com>
2019-04-17 21:11:09 +0300

    Update Runner.java

--- PR2/src/Runner.java
+++ PR2/src/Runner.java
@@ -3,7 +3,6 @@ import by.gsu.pmspr.Subject;
 
 public class Runner {
     public static void main(String[] args) {
-        int mass=0;
         Subject wire=new Subject(Material.STEEL,0.03);
         System.out.println(wire);
         wire.setMaterial(Material.COPPER);
